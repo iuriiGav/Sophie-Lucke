7d9361b76212d1c7f9f8342d91c3ea82e0afd53a
iuriiGav <iurii_gavryliuk@ukr.net>
2021-02-07 02:41:42 +0200

    before introducing pug refactoring

--- app.js
+++ app.js
@@ -3,6 +3,10 @@
 require('./jsplugins/flickity.min.css');
 require('./css/style.css');
 
+  // import Swiper JS
+  import Swiper from 'swiper';
+  // import Swiper styles
+  import 'swiper/swiper-bundle.css';
 
 window.$ = require("jquery");
 require("./jsplugins/flickity.pkgd.min.js")
