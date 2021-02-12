c98bf74720168fadc93af20e356884ad1e898a5a
iuriiGav <iurii_gavryliuk@ukr.net>
2021-02-12 12:44:03 +0200

    with webpack working

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 // webpack.config.js
 const path = require('path');
 const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const pug = {
   test: /\.pug$/,
@@ -21,10 +22,11 @@ const config = {
         rules: [
           pug,
           {
-            test: /\.css$/,
+            test: /\.(scss|css)$/,
             use: [
               'style-loader',
               'css-loader',
+              'sass-loader'
             ],
           },
           {
@@ -35,23 +37,53 @@ const config = {
           },
           {
             test: /\.(gif|svg|jpg|png)$/,
-            use: [
-              'file-loader',
-            ],
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images'
+            }
           },
+          {
+            test: /\.(ogg|mp3)$/,
+            // include: SRC,
+            loader: 'file-loader',
+            options: {
+              outputPath: 'audio'
+            }
+        }
         ],
       },
       plugins: [
         new HtmlWebpackPlugin({
-          filename: 'test-pug/index.html',
+          filename: 'index.html',
           template: 'pug-src/index.pug',
           inject: false,
         }),
         new HtmlWebpackPlugin({
-          filename: 'test-pug/index.html',
-          template: 'pug-src/index.pug',
+          filename: 'concerts.html',
+          template: 'pug-src/concerts.pug',
           inject: false
         }),
+        new HtmlWebpackPlugin({
+          filename: 'biography.html',
+          template: 'pug-src/biography.pug',
+          inject: false
+        }),
+        new HtmlWebpackPlugin({
+          filename: 'listen.html',
+          template: 'pug-src/listen.pug',
+          inject: false
+        }),
+        new HtmlWebpackPlugin({
+          filename: 'news.html',
+          template: 'pug-src/news.pug',
+          inject: false
+        }),
+        new HtmlWebpackPlugin({
+          filename: 'gallery.html',
+          template: 'pug-src/gallery.pug',
+          inject: false
+        }),
+        new CleanWebpackPlugin(),
     
      ]
 
